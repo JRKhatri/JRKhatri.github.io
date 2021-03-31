1c2db59af0be78dcc4c4df516eb1faaa6d3f1ee1
JRKhatri <79480451+JRKhatri@users.noreply.github.com>
2021-03-30 21:57:06 -0500

    Update myMochaTest.js

--- cs303/labW1D6/W2D2/myMochaTest.js
+++ cs303/labW1D6/W2D2/myMochaTest.js
@@ -1,13 +1,16 @@
 "use strict";
 const assert = require("assert"); 
 let textArray;
+let lengthFunc;
+
 describe("my map mocha test", function () {
     beforeEach(function () {
          textArray = ["John","Jack","Hi"];
+         lengthFunc = element => element.length;  
     });
 
     it("counts length of each array element", function () {
-        assert.deepEqual(myMap(textArray,function(elem){return elem.length;}), [4,4,2]);
+        assert.strictEqual(myMap(textArray,lengthFunc), [4,4,2]);
         assert.strictEqual(textArray, ["John","Jack","Hi"]);  //original array return;
     });
 });
