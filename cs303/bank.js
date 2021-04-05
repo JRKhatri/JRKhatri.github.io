ff24d1d2a5a896b2af2d034110151deb1c3dbddf
JRKhatri <79480451+JRKhatri@users.noreply.github.com>
2021-04-05 00:08:28 -0500

    Update bank.js

--- cs303/bank.js
+++ cs303/bank.js
@@ -16,24 +16,24 @@ bank.transactionDB =[
 {customerId: 3, customerTransaction:[5,-5,55]}
 ];
 
-bank.findcustomerId = function(bankId){
+bank.findCustomer = function(bankId){
     return bank.transactionDB.find(item => item.customerId===bankId);
 };
-console.log(bank.findcustomerId(3));
+//console.log(bank.findCustomer(3));
 
 bank.getBalance = function(bankId){
-    let customer = bank.findcustomerId(bankId);
+    let customer = this.findCustomer(bankId);
     let customerBalance = customer.customerTransaction.reduce((sumBalance, trans) => sumBalance + trans);
     return customerBalance;
 };
-console.log(bank.getBalance(3));
+//console.log(bank.getBalance(3));
 
 bank.debit = function(bankId, amount){
     const debitAmount = -  amount;
-    const customer= bank.findcustomerId(bankId);
-    const currentBalance = bank.getBalance(bankId);
-    const transaction =customer.customerTransaction;
-
+    const customer= this.findCustomer(bankId);
+    const currentBalance = this.getBalance(bankId);
+    const transaction = customer.customerTransaction;
+   
     if(amount > currentBalance){
         return ("Amount is less than current balance");
     } else {
@@ -41,19 +41,68 @@ bank.debit = function(bankId, amount){
     }
 
 };
-console.log(bank.debit(1,10));
-console.log(bank.findcustomerId(1));
-console.log(bank.getBalance(1));
+// console.log(bank.debit(1,10));
+// console.log(bank.findCustomer(1));
+// console.log(bank.getBalance(1));
 
 bank.credit = function (bankId, amount){
-    const customer = bank.findcustomerId(bankId);
-    const customerTrans = customer.customerTranasction;
-    console.log(customerTrans);
-   // customerTrans.push(amount);
-   
+    const customer= this.findCustomer(bankId);
+    if(amount < 0){
+        return console.log(`Invalid amount: ${amount}, enter correct amount.`);
+    }else{
+    customer.customerTransaction.push(amount);
+    } 
 };
-//console.log(bank.getBalance(1));
-bank.credit(1, 200);
-//console.log(bank.findcustomerId(1));
+// console.log(bank.getBalance(1));
+// bank.credit(1, -200);
+// console.log(bank.findCustomer(1));
+// console.log(bank.getBalance(1));
 
-//console.log(bank.getBalance(1));
+bank.saveTransaction = function(bankId, amount){
+    const customer = this.findCustomer(bankId);
+    customer.customerTransaction.push(amount);
+};
+//  bank.saveTransaction(3, 30);
+//  console.log(bank.findCustomer(3));
+
+
+
+bank.getBankBalance = function(){
+    const customerArr = bank.transactionDB;
+    let sum =0;
+    for(let prop of customerArr){
+        let propId = prop.customerId;
+        sum +=this.getBalance(propId);
+
+    }
+    return sum;
+};
+console.log(bank.getBankBalance());
+
+/**2. Recall the bank question from the first exam. The transactionsDB is publicly 
+ * accessible to any code that has access to the bank object. 
+ * Write a makeBank function that will encapsulate and return the bank object.
+ *  Make the transactionsDB private by making it a local variable in the makeBank 
+ * function instead of a property on the bank object.
+const bank = { transactionsDB: [],}; 
+bank.transactionsDB = [
+{ customerId: 1, customerTransactions: [10, 50, -40] }, 
+{ customerId: 2, customerTransactions: [10, 10, -10] },
+ { customerId: 3, customerTransactions: [5, -5, 55] }];
+
+bank.getBalance = function (id) {
+const customer = bank.transactionsDB.find(customer => customer.customerId === id); let balance = 0;
+for (const trans of customer.customerTransactions) { balance += trans; }
+return balance; };
+@returns {number} returns sum of all balances */ bank.bankBalance = function () { //IMPLEMENT THIS */
+
+    function makeBank() {
+        const transactionsDB = [
+            { customerId: 1, customerTransactions: [10, 50, -40] },
+            { customerId: 2, customerTransactions: [10, 10, -10] }];
+        const bank = {};
+        bank.debit = function (id, amount) {
+        bank.credit = function (id, amount) {
+        return bank;
+    }
+    const bank = makeBank();
